5d11b43af36082eab05a93ff5e692303135ece08
AlfonsoJR <aljimeruz@gmail.com>
2023-05-06 14:16:53 +0200

    2. Busqueda de ciudad v1

--- models/busquedas.js
+++ models/busquedas.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const maptilerClient = require('@maptiler/client');
 
 class Busquedas {
     historial = ['Madrid', 'Lucena', 'París'];
@@ -8,14 +9,31 @@ class Busquedas {
     }
 
     async ciudad(lugar = '') {
+        maptilerClient.config.apiKey = 'Gmp3jysz1OHeA0E8iEez';
+
         // petición http
         //console.log('Ciudad escrita: ', lugar)
         try {
+            //UTILIZANDO LA PROPIA LIBRERÍA DE MAPTILER
+            // const result = await maptilerClient.geocoding.forward(`${lugar}`, {
+            //     language: maptilerClient.geocoding.language.SPANISH,
+            // });
+            // result.features.forEach((e) => {
+            //     console.log(e.place_name, e.center);
+            // });
+
             const instance = axios.create({
                 baseURL: `https://api.maptiler.com/geocoding/ ${lugar}.json`,
+                params: {
+                    key: 'Gmp3jysz1OHeA0E8iEez',
+                    language: 'es',
+                    limit: 5,
+                },
             });
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const resp = await instance.get();
             console.log(resp.data);
+            // const resp = await axios.get('');
+            // console.log(resp.data);
             //return []; //retornar un array con los lugares que coincidan con lugar.
         } catch (error) {
             return [];

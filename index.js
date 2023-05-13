e33183cb37c25a4fbea5fc7a267d15ec13246f5e
Alfonso Jiménez <aljimeruz@gmail.com>
2023-05-13 14:17:30 +0200

    4. Variables de entorno

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
+require('dotenv').config();
+
 const { menuPrincipal, pausa, readInput } = require('./helpers/inquirer');
 const Busquedas = require('./models/busquedas');
 
+//console.log(process.env); //Gracias a dotenv se cargan las variables de entorno del archivo .env
 const main = async () => {
-    let opt = 0;
-    const busquedas = new Busquedas();
+  let opt = 0;
+  const busquedas = new Busquedas();
 
-    do {
-        opt = await menuPrincipal();
-        switch (opt) {
-            case 1:
-                // Mostrar mensaje
-                let lugar = await readInput('Ciudad: ');
-                await busquedas.ciudad(lugar);
+  do {
+    opt = await menuPrincipal();
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        let lugar = await readInput('Ciudad: ');
+        await busquedas.ciudad(lugar);
 
-                // Buscar los lugares
+        // Buscar los lugares
 
-                // Seleccionar el lugar
+        // Seleccionar el lugar
 
-                // Clima
+        // Clima
 
-                // Mostrar resultados
+        // Mostrar resultados
 
-                console.log('\nInformación de la ciudad\n'.green);
-                console.log('Ciudad:');
-                console.log('Lat:');
-                console.log('Lng:');
-                console.log('Temperatura:');
-                console.log('Mínima:');
-                console.log('Máxima');
-                break;
+        console.log('\nInformación de la ciudad\n'.green);
+        console.log('Ciudad:');
+        console.log('Lat:');
+        console.log('Lng:');
+        console.log('Temperatura:');
+        console.log('Mínima:');
+        console.log('Máxima');
+        break;
 
-            default:
-                break;
-        }
-        if (opt !== 0) await pausa();
-    } while (opt !== 0);
+      default:
+        break;
+    }
+    if (opt !== 0) await pausa();
+  } while (opt !== 0);
 };
 
 main();
--- models/busquedas.js
+++ models/busquedas.js
@@ -2,45 +2,45 @@ const axios = require('axios');
 const maptilerClient = require('@maptiler/client');
 
 class Busquedas {
-    historial = ['Madrid', 'Lucena', 'París'];
+  historial = ['Madrid', 'Lucena', 'París'];
 
-    constructor() {
-        // TODO: leer DB si existe
+  constructor() {
+    // TODO: leer DB si existe
+  }
+
+  get paramsMaptiler() {
+    return {
+      key: process.env.MAPTILER_KEY,
+      language: 'es',
+      limit: 5,
+    };
+  }
+  async ciudad(lugar = '') {
+    maptilerClient.config.apiKey = 'Gmp3jysz1OHeA0E8iEez';
+
+    // petición http
+    //console.log('Ciudad escrita: ', lugar)
+    try {
+      //UTILIZANDO LA PROPIA LIBRERÍA DE MAPTILER
+      // const result = await maptilerClient.geocoding.forward(`${lugar}`, {
+      //     language: maptilerClient.geocoding.language.SPANISH,
+      // });
+      // result.features.forEach((e) => {
+      //     console.log(e.place_name, e.center);
+      // });
+
+      const instance = axios.create({
+        baseURL: `https://api.maptiler.com/geocoding/ ${lugar}.json`,
+        params: this.paramsMaptiler,
+      });
+      const resp = await instance.get();
+      console.log(resp.data);
+
+      //return []; //retornar un array con los lugares que coincidan con lugar.
+    } catch (error) {
+      return [];
     }
-
-    get paramsMaptiler() {
-        return {
-            key: 'Gmp3jysz1OHeA0E8iEez',
-            language: 'es',
-            limit: 5,
-        };
-    }
-    async ciudad(lugar = '') {
-        maptilerClient.config.apiKey = 'Gmp3jysz1OHeA0E8iEez';
-
-        // petición http
-        //console.log('Ciudad escrita: ', lugar)
-        try {
-            //UTILIZANDO LA PROPIA LIBRERÍA DE MAPTILER
-            // const result = await maptilerClient.geocoding.forward(`${lugar}`, {
-            //     language: maptilerClient.geocoding.language.SPANISH,
-            // });
-            // result.features.forEach((e) => {
-            //     console.log(e.place_name, e.center);
-            // });
-
-            const instance = axios.create({
-                baseURL: `https://api.maptiler.com/geocoding/ ${lugar}.json`,
-                params: this.paramsMaptiler,
-            });
-            const resp = await instance.get();
-            console.log(resp.data);
-
-            //return []; //retornar un array con los lugares que coincidan con lugar.
-        } catch (error) {
-            return [];
-        }
-    }
+  }
 }
 
 module.exports = Busquedas;
